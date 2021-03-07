b6abc1c662fba0bb0facef86e415285ae9924e00
Davide Lo Cascio <davide@4eyes.ch>
2021-03-07 17:22:22 +0100

    implements dynamic api call

--- src/components/TestApi.js
+++ src/components/TestApi.js
@@ -1,25 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Timestamp from "react-timestamp";
 
 export default function TestApi() {
   const [stationboards, setStationboards] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [place, setPlace] = useState("");
+  const [station, setStation] = useState("");
+  const [apiURLBase, setApiURLBase] = useState(
+    "http://transport.opendata.ch/v1/stationboard?station="
+  );
 
-  useEffect(() => {
-    setLoading(true);
+  const getStations = () => {
+    let url = apiURLBase + place + "," + station + "&limit=10";
     axios
-      .get(
-        "http://transport.opendata.ch/v1/stationboard?station=Muttenz,Zum%20Park&limit=10"
-      )
+      .get(url)
       .then((response) => {
         setLoading(false);
-        console.log(response.data);
         setStationboards(response.data.stationboard);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  };
 
   return (
     <div>
@@ -27,8 +30,28 @@ export default function TestApi() {
         <div>is loading...</div>
       ) : (
         <div>
-          {stationboards.map((station) => (
-            <div>Abfahrt: {station.stop.departure}</div>
+          <input
+            type="text"
+            name="place"
+            placeholder="Ort"
+            onChange={(e) => setPlace(encodeURIComponent(e.target.value))}
+          />
+          <input
+            type="text"
+            name="station"
+            placeholder="Haltestelle"
+            onChange={(e) => setStation(encodeURIComponent(e.target.value))}
+          />
+          <button onClick={getStations}>Senden</button>
+          {stationboards.map((station, index) => (
+            <div key={index}>
+              <div>
+                Abfahrt:{" "}
+                <Timestamp relative date={station.stop.departureTimestamp} />
+              </div>
+              <div>Richtung: {station.to}</div>
+              <br />
+            </div>
           ))}
         </div>
       )}
